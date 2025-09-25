a37adf80a3e086aa8d0306c8c1d6184f3ccb9568
Ankit Kuamar <ankit.va1511@gmail.com>
2025-09-25 18:17:48 +0530

    just for testing

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar: React.FC<NavbarProps> = ({ className = '' }) => {
     }
     setIsMobileMenuOpen(false);
   };
-
+  // hey ankit
   return (
     <nav className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
       isScrolled 
